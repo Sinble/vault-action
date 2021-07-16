f99a9cabe168d1a72e537c932642357d6578bea3
Nick Bunn <nick_bunn@homedepot.com>
2021-07-16 16:18:18 -0500

    add gce as a approved method

--- dist/index.js
+++ dist/index.js
@@ -14612,7 +14612,7 @@ const got = __webpack_require__(77).default;
 const jsonata = __webpack_require__(350);
 const { auth: { retrieveToken }, secrets: { getSecrets } } = __webpack_require__(676);
 
-const AUTH_METHODS = ['approle', 'token', 'github', 'jwt', 'kubernetes'];
+const AUTH_METHODS = ['approle', 'token', 'github', 'jwt', 'kubernetes', 'gce'];
 
 async function exportSecrets() {
     const vaultUrl = core.getInput('url', { required: true });
